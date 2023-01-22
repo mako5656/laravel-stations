8172c559030309474d8f7251047cf3f7cf9136b6
Makoto Ikami <makoto.ikami@gmail.com>
2023-01-22 13:21:36 +0900

    RouteとControllerをどちらも使ってみよう

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-/*
-|-------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('practice', function() {
-    return response('practice');
-});
+use App\Http\Controllers\PracticeController;
 
-Route::get('practice2', function() {
-    $test = 'practice2';
-    return response($test);
-});
-
-Route::get('practice3', function() {
-    return response('test');
-});
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
